cfae66aff30b95138a2377b8fb7f340dea7ecd15
dryego barbosa <dryego.lisboa@gmail.com>
2023-03-26 19:13:03 -0300

    Coreçoes.

--- 01/src/controlador/envioEmails.js
+++ 01/src/controlador/envioEmails.js
@@ -1,18 +1,37 @@
+const { knex } = require('../conexao');
+const compiladorHTML = require('../utils/compiladorHTML');
 const transportador = require('./email');
+
 const envioEmails = async (req, res) => {
+    const { texto } = req.body;
 
-    //envio de email
+    try {
+        const emails = await knex('email');
 
-    transportador.sendMail({
-        from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
-        to: `${usuario.nome} <${usuario.email}>`,
-        subject: 'verificação de Integração',
-        text: 'Integração verificada com sucesso!'
-    });
+        console.log(emails);
 
-    return res.json('Login efetuado com sucesso.');
+        //envio de email
+        for (const email of emails) {
+
+            const htmlNewsLetter = await compiladorHTML('./src/templates/newsletter.html', {
+                usuario: email.nome,
+                texto: texto
+            })
+            transportador.sendMail({
+                from: `${process.env.EMAIL_NAME} <${process.env.EMAIL_FROM}>`,
+                to: `${email.nome} <${email.email}>`,
+                subject: 'Atualizações e novidades',
+                html: htmlNewsLetter
+            });
+        }
+
+        return res.status(200).json('Emails enviados com sucesso.');
+
+    } catch (error) {
+        return res.status(500).json('Erro no servidor.')
+    }
+
 }
-
 module.exports = {
     envioEmails
 }
--- 01/src/rotas.js
+++ 01/src/rotas.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { cadastrousuario } = require('./controlador/cadastro');
+const { envioEmails } = require('./controlador/envioEmails');
 
 const rotas = express.Router();
 
 rotas.post('/', cadastrousuario);
+rotas.get('/envio', envioEmails);
 
 module.exports = { rotas };
